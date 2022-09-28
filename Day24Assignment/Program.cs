ddaca1f2311d5b624b755bfa1e162b341893ead3
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-28 17:28:57 +0530

    [Roshan]UC3-Valid Email

--- Day24Assignment/Program.cs
+++ Day24Assignment/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Chosee a oprtion");
             Console.WriteLine("1. UC1 - Validate First Name");
             Console.WriteLine("2. UC2 - Validate Last Name");
+            Console.WriteLine("3. UC3 - Validate Email Address");
 
             int option = Convert.ToInt32(Console.ReadLine());
 
@@ -19,9 +20,15 @@
                 case 2:
                     ValidateLastName2.ValidateLastName();
                     break;
+                case 3:
+                    ValidateEmail1.ValidateEmail();
+                    break;
                 default:
                     Console.WriteLine("Choose a appropriate option");
                     break;
+                
+
+
 
             }
         }
---
+++ Day24Assignment/ValidateEmail1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day24Assignment
+{
+    internal class ValidateEmail1
+    {
+        public static void ValidateEmail()
+        {
+            
+            
+                Console.WriteLine("Enter a email");
+                string email = Console.ReadLine();
+
+                string pattern = @"^([a-z]+)(\.[a-z0-9_\+\-]+)?@([a-z]+)\.([a-z]{2,4})(\.[a-z]{2})?$";
+                if (Regex.IsMatch(email, pattern))
+                {
+                    Console.WriteLine("Email is " + email);
+                }
+                else
+                {
+                   Console.WriteLine ("Email is not valid ");
+                }
+           
+        }
+    }
+}
