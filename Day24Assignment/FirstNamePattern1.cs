5da89d6623c462a3cdd96a4d7bd297033ad1bb74
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-28 17:14:45 +0530

    [Roshan]Valid First Name

---
+++ Day24Assignment/FirstNamePattern1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day24Assignment
+{
+    internal class FirstNamePattern1
+    {
+        public static void ValidateFirstName()
+        {
+            
+            
+                Console.WriteLine("Enter First Name");
+                String firstName = Console.ReadLine();
+                string pattern = "^[A-Z]{1}[a-z]{2,}$";
+                if (Regex.IsMatch(firstName, pattern))
+                {
+                    Console.WriteLine("First Name : " + firstName);
+                }
+                else
+                {
+                   Console.WriteLine ("First name should be first letter capital and more then 2 characters");
+                }
+            
+            
+           
+
+        }
+    }
+}
---
+++ Day24Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Day24Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chosee a oprtion");
+            Console.WriteLine("1. UC1 - Validate First Name");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    FirstNamePattern1.ValidateFirstName();
+                    break;
+                default:
+                    Console.WriteLine("Choose a appropriate option");
+                    break;
+
+            }
+        }
+    }
+}
