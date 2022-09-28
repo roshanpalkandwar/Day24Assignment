9bd22c8b6488e3fc509578013790e0f73df6819a
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-28 17:40:03 +0530

    [Roshan]UC5-PassWord Rule1

---
+++ Day24Assignment/PassWordRule1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day24Assignment
+{
+    internal class PassWordRule1
+    {
+        public static void ValidatePassword()
+        {
+            
+            
+                Console.WriteLine("Enter Password");
+                string password = Console.ReadLine();
+                var hasLowerChar = new Regex("^([a-z]){8,}$");
+
+                if (!hasLowerChar.IsMatch(password))
+                {
+                    Console.WriteLine ("Password is not valid... Password should have minimum 8 characters");
+                }
+                else
+                {
+                    Console.WriteLine("Password : " + password);
+                }
+            
+        }
+    }
+}
--- Day24Assignment/Program.cs
+++ Day24Assignment/Program.cs
@@ -9,6 +9,7 @@
             Console.WriteLine("2. UC2 - Validate Last Name");
             Console.WriteLine("3. UC3 - Validate Email Address");
             Console.WriteLine("4. UC4 - Validate Mobile Number");
+            Console.WriteLine("5. UC5 - Validate password Rule1 - Password should have minimum 8 characters");
 
             int option = Convert.ToInt32(Console.ReadLine());
 
@@ -26,6 +27,9 @@
                     break;
                 case 4:
                     ValidateMobileNumber1.ValidateMobileNumber();
+                    break;
+                case 5:
+                    PassWordRule1.ValidatePassword();
                     break;
                 default:
                     Console.WriteLine("Choose a appropriate option");
